db08691023d7e8515a30a0414f29bf8890ff0718
Jose Lopez <josei.lopez.velez@gmail.com>
2025-09-07 20:34:09 -0500

    - Se agrega la capacidad para listar estudiantes y guardarlos por medio de Spring data. - Se crean los endpoints necesarios para exponer dicha funcionalidad por medio de una api. - Se agrega OpenApi para permitir consumir la api via Swagger - Se modifica el archivo docker-compose para usar la última imagen de MySQL y se modifica el puerto por el que se expone la base de datos para evitar conflictos con instalaciones locales de la herramienta. - Se modifica la api para gestionar CORS. - Se modifica el frontend para obtener la url de la api por medio de un archivo de ambiente.

--- frontend/src/app/services/estudiantes.service.ts
+++ frontend/src/app/services/estudiantes.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Estudiante } from "../models/estudiante.model";
+import { environment } from "../../environments/environment";
 
 @Injectable({ providedIn: 'root' })
 export class EstudiantesService {
   // Thanks to proxy.conf.json, calls to "/api" go to http://localhost:8080
-  private readonly base = '/api/estudiantes';
+  private readonly base = `${environment.apiUrl}/estudiantes`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   getAll(): Observable<Estudiante[]> {
     return this.http.get<Estudiante[]>(this.base);
